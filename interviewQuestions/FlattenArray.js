62557a81b56934062cf90d437471bfe9e3e6661a
Nest <nsitseo@gmail.com>
2025-09-27 00:45:17 +0530

    added interview questions

---
+++ interviewQuestions/FlattenArray.js
@@ -0,0 +1,28 @@
+const input = [1, 4, 6, [7, [5, 8]], 9];
+let resultArray = [];
+const recursiveFunction = (arr) => {
+  arr.forEach((element) => {
+    if (typeof element === "number") {
+      resultArray.push(element);
+    } else {
+      recursiveFunction(element);
+    }
+  });
+};
+recursiveFunction(input);
+
+console.log(resultArray);
+
+console.log("Start");
+Promise.resolve(
+  (() => {
+    return "Step1";
+  })()
+)
+  .then((value) => console.log(value))
+  .then((value) => {
+    console.log(value);
+    return "Step3";
+  })
+  .then((value) => console.log(value));
+console.log("End");
