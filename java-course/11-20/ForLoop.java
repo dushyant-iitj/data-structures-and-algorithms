223691630bc22cba6fea92f10f3b29a6bd46894c
nest <nsitseo@gmail.com>
2026-06-11 00:11:39 +0530

    N-007: 11-20 completed

---
+++ java-course/11-20/ForLoop.java
@@ -0,0 +1,12 @@
+public class ForLoop {
+  public static void main(String[] args) {
+
+    for (int i = 1; i <= 7; i++) {
+      System.out.println("Day " + i);
+      for (int j = 9; j <= 17; j++) {
+        System.out.println("  " + (j) + " - " + (j + 1));
+      }
+    }
+
+  }
+}
